28a4e349b2cb0988b1e877326c305fbd2477e4dd
Md Rashid Alam <156087283+rashid9110@users.noreply.github.com>
2024-01-10 19:22:35 +0530

    Delete generateNumberBetweenAnyTwoNumber.js

--- generateNumberBetweenAnyTwoNumber.js
+++
@@ -1,9 +0,0 @@
-let prompt=require('prompt-sync')();
-let num1=prompt("Enter a first smill number :")
-num1=parseInt(num1)
-let num2=prompt("Enter a second big number :")
-num2=parseInt(num2)
-console.log("Output number is :")
-for(let i=num1+1;i<=num2;i++){
-    console.log(i)
-}
