d313efe3be418b576124d4cb52d81f8c7758bf27
Md Rashid Alam <156087283+rashid9110@users.noreply.github.com>
2024-01-10 19:22:19 +0530

    Delete chackNumberIsPositive_Negative_zero.js

--- chackNumberIsPositive_Negative_zero.js
+++
@@ -1,4 +0,0 @@
-let prompt=require('prompt-sync')();
-let a = prompt("Enter a number :")
-a=parseInt(a)
-a>0?console.log(a+" is a positive number"): a<0 ? console.log(a+" is a negative number"):  console.log("This number is zero")
